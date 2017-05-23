dd166860a737f91e0acacf05ed0f12e0c36058fb
Nhat Ton <minhnhattonthat@gmail.com>
2017-05-24 00:34:13 +0700

    Test opening youtube link in browser

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const anchorme = require('anchorme')
 const app = express()
+const opn = require('opn');
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
@@ -39,16 +39,15 @@ app.post('/webhook/', function (req, res) {
 	    let sender = event.sender.id
 	    if (event.message && event.message.text) {
 		    let text = event.message.text
-		    if (text === 'Generic') {
-			    sendGenericMessage(sender)
-		    	continue
-		    }
+		    
 			var id = getYoutubeVideoId(text)
 			if (id) {
+				opn(text, {app: 'chrome'});
 				sendTextMessage(sender, "This is the id: " + id)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender, "Hong phải link youtube ahihi đồ ngốc!")
+			// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 	    }
     }
     res.sendStatus(200)
@@ -80,55 +79,6 @@ function sendTextMessage(sender, text) {
 
 function requestMusic(sender, url) {
 
-}
-
-function sendGenericMessage(sender) {
-    let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
-				"template_type": "generic",
-			    "elements": [{
-					"title": "First card",
-				    "subtitle": "Element #1 of an hscroll",
-				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-				    "buttons": [{
-					    "type": "web_url",
-					    "url": "https://www.messenger.com",
-					    "title": "web url"
-				    }, {
-					    "type": "postback",
-					    "title": "Postback",
-					    "payload": "Payload for first element in a generic bubble",
-				    }],
-			    }, {
-				    "title": "Second card",
-				    "subtitle": "Element #2 of an hscroll",
-				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-				    "buttons": [{
-					    "type": "postback",
-					    "title": "Postback",
-					    "payload": "Payload for second element in a generic bubble",
-				    }],
-			    }]
-		    }
-	    }
-    }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-	    json: {
-		    recipient: {id:sender},
-		    message: messageData,
-	    }
-    }, function(error, response, body) {
-	    if (error) {
-		    console.log('Error sending messages: ', error)
-	    } else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
 }
 
 function getYoutubeVideoId(url){
