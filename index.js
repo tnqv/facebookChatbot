a525efaf1170674afb5c06211a08ee4c7568191b
Nhat Ton <minhnhattonthat@gmail.com>
2017-06-12 22:02:06 +0700

    Refactor

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const opn = require("opn");
 const vol = require("vol");
 const mongodb = require("mongodb");
 const sendMessage = require("./send_message.js");
-//----socket io 
+//----socket io
 const http = require("http").createServer(app);
 const io = require("socket.io").listen(http);
+const fs = require("fs");
 
-const fs = require('fs');
-const token =
-    process.env.FB_PAGE_ACCESS_TOKEN ||
-    "EAAUdTbCZA0aoBAIlqZAC87lacdfgWoFyySJhJJ9LEFbZA2paNyT2o5hLPtmwOI9hsyWQy8hXOLShPA2aN2WIn9yx4BuPv2y6cjmzZBzDZCWUKE0qUrcImiEptea7OGOpWZBm29R0cXApZBd0DFLu9ye8rZAzPA534ZAVW01dPBicICgZDZD";
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
 const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3";
 
@@ -22,55 +20,48 @@ const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 const API_ZING_MP3_GETLINK_URL = "http://api.mp3.zing.vn/api/mobile/song/getsonginfo?requestdata=";
 
-// https://www.googleapis.com/youtube/v3/videos?id=9bZkp7q19f0&part=contentDetails&key=
-
-//var mongodbUrl = 'mongodb://tnqv102:Bikhung101@ds111922.mlab.com:11922/heroku_shxqcpv3'
 const mongodbUrl = process.env.MONGOLAB_URI;
 
 var MongoClient = mongodb.MongoClient;
-// var assert = require('assert');
-// MongoClient.connect(mongodbUrl, function (err, db) {
-//     assert.equal(null, err);
-//     console.log("Connect to server successfully");
-//     db.close();
-// });
-var socketClient = null;
-io.on('connection', function(client) {
-    socketClient = client;
-    console.log('Client connected...');
-    client.emit('messages', { msg: 'Hello from server'});
 
-    client.on('join', function(data) {
+var socketClient = null;
+io.on("connection", function(client) {
+    socketClient = client;
+    console.log("Client connected...");
+    client.emit("messages", { msg: "Hello from server" });
+
+    client.on("join", function(data) {
         console.log(data);
     });
-
 });
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.MESSENGER_PORT);
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 
 // Process application/json
 app.use(bodyParser.json());
 
 // Index route
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
     res.send("Hello world, I am a chat bot hihi");
 });
 
-app.get("/youtube/", function (req, res) {
+app.get("/youtube/", function(req, res) {
     res.sendFile("/youtube.html", { root: __dirname });
-})
+});
 // app.get("/client-control/",function(req,res){
 //     res.sendFile("/control.html", { root: __dirname });
 // })
-app.post("/client-control/",function(req,res){
+app.post("/client-control/", function(req, res) {
     res.sendFile("/control.html", { root: __dirname });
-})
+});
 // for Facebook verification
-app.get("/webhook/", function (req, res) {
+app.get("/webhook/", function(req, res) {
     if (
         req.query["hub.mode"] === "subscribe" &&
         req.query["hub.verify_token"] === "my_voice_is_my_password_verify_me"
@@ -82,18 +73,18 @@ app.get("/webhook/", function (req, res) {
     res.sendStatus(403);
 });
 
-app.post("/webhook/", function (req, res) {
+app.post("/webhook/", function(req, res) {
     var data = req.body;
 
     // Make sure this is a page subscription
     if (data.object === "page") {
         // Iterate over each entry - there may be multiple if batched
-        data.entry.forEach(function (entry) {
+        data.entry.forEach(function(entry) {
             var pageID = entry.id;
             var timeOfEvent = entry.time;
 
             // Iterate over each messaging event
-            entry.messaging.forEach(function (event) {
+            entry.messaging.forEach(function(event) {
                 if (event.message) {
                     console.log(entry.id);
                     receivedMessage(event);
@@ -117,12 +108,12 @@ app.post("/webhook/", function (req, res) {
 function insertSongToDatabase(songUrl) {
     console.log("song url", songUrl);
     MongoClient.connect(mongodbUrl, (err, db) => {
-        db.collection('songs').count().then(count => {
+        db.collection("songs").count().then(count => {
             if (count < 10) {
-                db.collection('songs').insert({ 'song_url': songUrl }).then((result)=>{
-                        db.close();
+                db.collection("songs").insert({ song_url: songUrl }).then(result => {
+                    db.close();
                 });
-            }else db.close();
+            } else db.close();
         });
     });
 }
@@ -145,132 +136,125 @@ function receivedMessage(event) {
     var messageText = message.text;
     var messageAttachments = message.attachments;
 
-    getFbName(senderID).then(first_name => {
-        if (messageText) {
-            // Set nickname
-            switch (senderID) {
-                case '1176661302462412':
-                    first_name = "Bà già xài Zalo";
-                    break;
-                case '1370107956411784':
-                    first_name = "Zú";
-                    break;
-                case '738286506296286':
-                    first_name = "chị đại";
-                    break;
-                case '1067670383333151':
-                    first_name = "Tuấn tre trau";
-                    break;
-                case '1447875711972194':
-                    first_name = "a Việt";
-                    break;
-                case '1843143285699478':
-                    first_name = "admin";
-                    break;
-                case '1218099261649369':
-                    first_name = "Cu Tũn";
-                    break;
-                default:
-                    break;
-            }
-
-            //Check & open youtube link
-            
-            var videoId = getYoutubeVideoId(messageText);
-            if (videoId) {
-                // , "music"
-                //opn(messageText, "music");
-                // getYoutubeDuration(videoId);
-                console.log("insert db", videoId);
-                if (socketClient != null){
-                    socketClient.emit('songs',{msg : videoId, videoType : 'youtube'});
+    getFbName(senderID)
+        .then(first_name => {
+            if (messageText) {
+                // Set nickname
+                switch (senderID) {
+                    case "1176661302462412":
+                        first_name = "Bà già xài Zalo";
+                        break;
+                    case "1370107956411784":
+                        first_name = "Zú";
+                        break;
+                    case "738286506296286":
+                        first_name = "Chị Đại";
+                        break;
+                    case "1067670383333151":
+                        first_name = "Tuấn Trẻ Trâu";
+                        break;
+                    case "1447875711972194":
+                        first_name = "a Việt";
+                        break;
+                    case "1843143285699478":
+                        first_name = "admin";
+                        break;
+                    case "1218099261649369":
+                        first_name = "Cu Tũn";
+                        break;
+                    case "1434187019953992":
+                        first_name = "Hoàng";
+                        break;
+                    default:
+                        break;
                 }
 
-                insertSongToDatabase(messageText);
-                sendMessage.sendTextMessage(senderID, "Ô kê quẩy lên " + first_name + "!!");
-                return;
-            }
-            
-            
+                //Check & open youtube link
 
-            //Check & open zing mp3 link
-            if (messageText.includes("mp3.zing.vn")) {
-                //opn(messageText, "music");
-                console.log("mp3 ne` hehehe",messageText);
-                console.log(getMP3VideoId(messageText));
-                if (socketClient != null){
-                        socketClient.emit('songs',{msg : videoId, videoType : 'mp3'});
+                var videoId = getYoutubeVideoId(messageText);
+                if (videoId) {
+                    // , "music"
+                    //opn(messageText, "music");
+                    // getYoutubeDuration(videoId);
+                    console.log("insert db", videoId);
+                    if (socketClient != null) {
+                        socketClient.emit("songs", { msg: videoId, videoType: "youtube" });
+                    }
+
+                    insertSongToDatabase(messageText);
+                    sendMessage.sendTextMessage(senderID, "Ô kê quẩy lên " + first_name + "!!");
+                    return;
                 }
-                sendMessage.sendTextMessage(
-                    senderID,
-                    "Ô kê quẩy luôn " + first_name + "!!"
-                );
-                return;
-            }
 
-            if(messageText.includes("mute") 
-                ||  messageText.includes("nín") 
-                ||  messageText.includes("câm")){
-                if(socketClient != null){
-                    socketClient.emit("mute",{});
+                //Check & open zing mp3 link
+                if (messageText.includes("mp3.zing.vn")) {
+                    //opn(messageText, "music");
+                    console.log("mp3 ne` hehehe", messageText);
+                    console.log(getMP3VideoId(messageText));
+                    if (socketClient != null) {
+                        socketClient.emit("songs", { msg: videoId, videoType: "mp3" });
+                    }
+                    sendMessage.sendTextMessage(senderID, "Ô kê quẩy luôn " + first_name + "!!");
+                    return;
                 }
-                
-            }
 
-            if(messageText.includes("unmute")){
-                if(socketClient != null){
-                    socketClient.emit("unmute",{});
+                if (
+                    messageText.includes("mute") ||
+                    messageText.includes("nín") ||
+                    messageText.includes("câm")
+                ) {
+                    if (socketClient != null) {
+                        socketClient.emit("mute", {});
+                    }
                 }
-                
-            }
 
-            if(messageText.includes("dừng")||
-                messageText.includes("stop")){
-                if(socketClient != null){
-                    socketClient.emit("stop",{});
-                }    
-                
-            }
-
-            if(messageText.includes("resume")){
-                if(socketClient != null){
-                    socketClient.emit("resume",{});
+                if (messageText.includes("unmute")) {
+                    if (socketClient != null) {
+                        socketClient.emit("unmute", {});
+                    }
                 }
-                
-            }
 
-            if(messageText.includes("set volumn")){
+                if (messageText.includes("dừng") || messageText.includes("stop")) {
+                    if (socketClient != null) {
+                        socketClient.emit("stop", {});
+                    }
+                }
+
+                if (messageText.includes("resume")) {
+                    if (socketClient != null) {
+                        socketClient.emit("resume", {});
+                    }
+                }
+
+                if (messageText.includes("set volumn")) {
                     let arrayMessage = new Array();
                     arrayMessage = messageText.split(" ");
                     let volumnNumber = parseInt(arrayMessage[2]);
-                    if(socketClient != null){
-                        socketClient.emit("setVolumn",{volumnOption : volumnNumber});
+                    if (socketClient != null) {
+                        socketClient.emit("setVolumn", { volumnOption: volumnNumber });
                     }
-                    
-            }
-            
+                }
 
-            //Default text
-            switch (messageText) {
-                case "generic":
-                    sendMessage.sendGenericMessage(senderID);
-                    break;
-                case "volumn":
-                    sendMessage.sendQuickReplyMessage(senderID);
-                    break;
-                default:
-                    sendMessage.sendTextMessage(
-                        senderID,
-                        "Hong phải link youtube/zing mp3 ahihi " +
-                        first_name +
-                        " ngốc!"
-                    );
-                    break;
+                //Default text
+                switch (messageText) {
+                    case "generic":
+                        sendMessage.sendGenericMessage(senderID);
+                        break;
+                    case "volumn":
+                        sendMessage.sendQuickReplyMessage(senderID);
+                        break;
+                    default:
+                        sendMessage.sendTextMessage(
+                            senderID,
+                            "Hong phải link youtube/zing mp3 ahihi " + first_name + " ngốc!"
+                        );
+                        break;
+                }
+            } else if (messageAttachments) {
+                // sendTextMessage(senderID, "Message with attachment received");
             }
-        } else if (messageAttachments) {
-            // sendTextMessage(senderID, "Message with attachment received");
-        }
-    }).catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 }
 
 function receivedPostback(event) {
@@ -283,8 +267,7 @@ function receivedPostback(event) {
     var payload = event.postback.payload;
 
     console.log(
-        "Received postback for user %d and page %d with payload '%s' " +
-        "at %d",
+        "Received postback for user %d and page %d with payload '%s' " + "at %d",
         senderID,
         recipientID,
         payload,
@@ -309,9 +292,8 @@ function receivedPostback(event) {
 //     )
 // }
 
-
 // Spin up the server
-app.listen(app.get("port"), function () {
+app.listen(app.get("port"), function() {
     console.log("running on port", app.get("port"));
 });
 
@@ -319,15 +301,16 @@ http.listen(5001);
 
 function getFbName(senderID) {
     return new Promise((resolve, reject) => {
-        request({
-            url: "https://graph.facebook.com/v2.9/" + senderID,
-            qs: {
-                fields: "first_name",
-                access_token: token
+        request(
+            {
+                url: "https://graph.facebook.com/v2.9/" + senderID,
+                qs: {
+                    fields: "first_name",
+                    access_token: token
+                },
+                method: "GET"
             },
-            method: "GET"
-        },
-            function (error, response, body) {
+            function(error, response, body) {
                 if (error) {
                     console.log("Error request name: ", error);
                     reject(error);
@@ -351,9 +334,9 @@ function getYoutubeVideoId(url) {
     return match && match[7].length == 11 ? match[7] : false;
 }
 
-function getMP3VideoId(url){
+function getMP3VideoId(url) {
     let splitStrArrFromUrl = url.split("/");
     let secondSplit = splitStrArrFromUrl[5].split(".");
     console.log("url Zing", url);
-    return secondSplit[0] && secondSplit[0].length == 8? secondSplit[0] : false;
+    return secondSplit[0] && secondSplit[0].length == 8 ? secondSplit[0] : false;
 }
